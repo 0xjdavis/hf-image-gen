c36c6a2aab81734da7bd3a94bdfb916bdfe4df2a
J. Davis <98430977+0xjdavis@users.noreply.github.com>
2024-08-05 19:40:31 -0700

    Initial commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ def text2speech(message):
     model = SpeechT5ForTextToSpeech.from_pretrained("microsoft/speecht5_tts")
     vocoder = SpeechT5HifiGan.from_pretrained("microsoft/speecht5_hifigan")
     
-    inputs = processor(text="My dog is so cute.", return_tensors="pt")
+    inputs = processor(text=message, return_tensors="pt")
     speaker_embeddings = torch.zeros((1, 512))  # or load xvectors from a file
     
     set_seed(555)  # make deterministic
