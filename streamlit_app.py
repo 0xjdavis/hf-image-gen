7b0101a9bb67a645fb926f295195088a138f7ccf
J. Davis <98430977+0xjdavis@users.noreply.github.com>
2024-08-05 19:34:03 -0700

    Initial commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,14 +61,14 @@ def main():
         st.image(uploaded_file, caption="Uploaded Image", use_column_width=True)
         scenario = img2text(image_path)
         story = generateStory(scenario)
-        text2speech(story)
+        text2speech(message)
 
          
         with st.expander("Scenario"):
             st.write(scenario)
         with st.expander("Story"):
             st.write(story)
-        audio_bytes = text2speech(story)
+        audio_bytes = text2speech(message)
         st.audio(audio_bytes, format='audio/mp3')
 
 if __name__ == "__main__":
