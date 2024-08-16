4b747fef9f3f951c881fba3b7b0b600729da24a5
J. Davis <98430977+0xjdavis@users.noreply.github.com>
2024-08-16 00:11:35 -0700

    Initial commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,8 +49,38 @@ def text2speech(message):
     return audio_path
 
 def main():
-    st.set_page_config(page_title="Image to Story", page_icon="🤖")
-    st.header("Upload an image to create an audio story!")
+        
+    # Setting page layout
+    st.set_page_config(
+        page_title="Generate Stories from Images with Hugging Face Transformers",
+        page_icon="✨",
+        layout="centered",
+        initial_sidebar_state="expanded"
+    )
+    
+    # Sidebar for API Key and User Info
+    st.sidebar.header("About App")
+    st.sidebar.markdown('This is an app to convert rasterized images (jpeg, png) to a pixelized format created by <a href="https://ai.jdavis.xyz" target="_blank">0xjdavis</a>.', unsafe_allow_html=True)
+     
+    # Calendly
+    st.sidebar.markdown("""
+        <hr />
+        <center>
+        <div style="border-radius:8px;padding:8px;background:#fff";width:100%;">
+        <img src="https://avatars.githubusercontent.com/u/98430977" alt="Oxjdavis" height="100" width="100" border="0" style="border-radius:50%"/>
+        <br />
+        <span style="height:12px;width:12px;background-color:#77e0b5;border-radius:50%;display:inline-block;"></span> <b>I'm available for new projects!</b><br />
+        <a href="https://calendly.com/0xjdavis" target="_blank"><button style="background:#126ff3;color:#fff;border: 1px #126ff3 solid;border-radius:8px;padding:8px 16px;margin:10px 0">Schedule a call</button></a><br />
+        </div>
+        </center>
+        <br />
+    """, unsafe_allow_html=True)
+    
+    # Copyright
+    st.sidebar.caption("©️ Copyright 2024 J. Davis")
+
+    st.title("Generate Stories from Images with Hugging Face Transformers")
+    st.write("Upload an image to create an audio story!")
     uploaded_file = st.file_uploader("Upload an image", type="png")
 
     if uploaded_file is not None:
